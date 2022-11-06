5bd51b6e53c569e50f50dc4d3ec1815eceb6be6d
Austen Constable <awconstable@gmail.com>
2022-11-06 14:51:58 +0000

    Conversation street

---
+++ Speak.py
@@ -0,0 +1,18 @@
+print("What is your name?")
+
+name = input()
+
+
+def output_name(n):
+    print("Hello " + n)
+
+
+
+output_name(name)
+
+print("What is your favourite colour?")
+
+colour = input()
+
+
+print("Cool, Bye!!")
